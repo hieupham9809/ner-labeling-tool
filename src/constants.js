6f3a0d7a1577394e6ad2617f31b7f46e5128f7a9
caochanhduong <caochanhduongbku@gmail.com>
2020-01-05 15:26:30 +0700

    post request ppredict

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,2 @@
-export const PREDICT_API = 'https://real-estate-extraction.herokuapp.com/api';
+// export const PREDICT_API = 'https://real-estate-extraction.herokuapp.com/api';
+export const PREDICT_API = 'http://127.0.0.1:5000/api/tagging/v1';
